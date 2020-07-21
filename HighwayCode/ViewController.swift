ff57fbea4317c030ea4e4d5b04b3861247042708
Andrew Visotskiy <firemaaaan@gmail.com>
2020-07-21 11:12:45 +0300

    Create empty project

---
+++ HighwayCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HighwayCode
+//
+//  Created by Andrew Visotskyy on 21.07.2020.
+//  Copyright © 2020 Gagnant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
